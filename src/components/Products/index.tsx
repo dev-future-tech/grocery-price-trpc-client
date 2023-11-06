be55356334b2c7de549e471b1f5c884ac9594247
Anthony Ikeda <anthony.ikeda@gmail.com>
2023-11-06 14:38:11 -0600

    Added missing code

---
+++ src/components/Products/index.tsx
@@ -0,0 +1,12 @@
+import { FunctionComponent } from "react";
+
+export type ProductProps = {
+    limit?: number;
+};
+
+export const Product: FunctionComponent<ProductProps> = ({ limit }: ProductProps ) => {
+    return (
+        <>
+        </>
+    )
+}
