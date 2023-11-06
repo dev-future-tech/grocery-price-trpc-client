1780c0edb4dbd26faa5daa97ce9c93fcfaa61f5a
Anthony Ikeda <anthony.ikeda@gmail.com>
2023-11-06 22:03:46 +0000

    Testing in another environment

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,12 +1,42 @@
+import {
+    QueryClient,
+    QueryClientProvider,
+    useQuery
+} from '@tanstack/react-query';
 import { FunctionComponent } from "react";
+
+const queryClient = new QueryClient();
 
 export type ProductProps = {
     limit?: number;
 };
 
-export const Product: FunctionComponent<ProductProps> = ({ limit }: ProductProps ) => {
+export default function Product ({ limit }: ProductProps ) {
     return (
-        <>
-        </>
-    )
+        <QueryClientProvider client={queryClient}>
+            <ProductDetail />
+        </QueryClientProvider>
+    );
+}
+
+function ProductDetail() {
+    const { isPending, error, data } = useQuery({
+        queryKey: ['repoData'],
+        queryFn: () =>
+            fetch('https://localhost:3000/getProducts').then(
+                (res) => res.json(),
+            ),
+    })
+
+    if (isPending) return "Loading...";
+
+    if(error) return 'An error has occurred: ' + error.message
+
+    return (
+        <div>
+            <h1>{data.product_name}</h1>
+            <p>{data.description}</p>
+            <strong>{data.sku}</strong>
+        </div>
+    );
 }
